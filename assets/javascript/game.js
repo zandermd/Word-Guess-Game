e5477335850eebd900abcb0fda772738d7229af0
Zander Dyer <zanderdyer@Zanders-MacBook-Pro.local>
2018-11-13 19:50:23 -0700

    first push

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// create an array of words
+const words = ['california','utah','colorado','arizona','florida','georgia'];
+// choose words randomly
+let choosenWord = math.floor(math.random() * words.length);
+console.log(choosenWord);
+// create underscores based on length of word
+// get users guess
+// check if guess is right
+// if right push to right array 
+// if wrong push to wrong array 
